f5da53377552051a7ed64ad5d2f05b90d28653bc
alissonbms <alissonnewdev@gmail.com>
2025-03-23 08:45:07 -0300

    chore: add express.json() middleware to allow the application to parse JSON data.

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,8 @@ import connectToDB from "./lib/database/db.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
