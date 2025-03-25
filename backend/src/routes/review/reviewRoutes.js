4d7371041bebd0f4718bc0b9e2e0929c278a1988
alissonbms <alissonnewdev@gmail.com>
2025-03-24 21:39:43 -0300

    feat: create post route that add a new review to database.

--- backend/src/routes/review/reviewRoutes.js
+++ backend/src/routes/review/reviewRoutes.js
@@ -45,4 +45,46 @@ router.get("/", protectRoute, async (req, res) => {
   }
 });
 
+router.post("/", protectRoute, async (req, res) => {
+  try {
+    const { title, bookTitle, rating, image, caption } = req.body;
+
+    if (!title || !bookTitle || !rating || !image || !caption) {
+      return res.status(400).json({
+        message: "Please fill in all fields.",
+      });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(image, {
+      folder: "voci-letterarie/reviews",
+      unique_filename: true,
+    });
+
+    const imageUrl = uploadResponse.secure_url;
+
+    const newReview = await Review.create({
+      title,
+      bookTitle,
+      rating,
+      image: imageUrl,
+      caption,
+      user: req.user._id,
+    });
+
+    if (!newReview) {
+      console.log(
+        "Error in review creation route, when trying to create a new review.",
+      );
+      return res.status(500).json({ message: "Internal server error." });
+    }
+
+    await newReview.save();
+
+    return res.status(201).json(newReview);
+  } catch (error) {
+    console.log("Error in review creation route.", error);
+    return res.status(500).json({ message: "Internal server error." });
+  }
+});
+
 export default router;
